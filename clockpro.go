f70a82bd3cf03a41114d0a9925cc2828a25181e3
Damian Gryski <damian@gryski.com>
2014-08-03 01:44:53 +0200

    Move to container/ring
    This reduces lots of bookkeeping we had to do w/r/t offsets.  We were
    simulating a circular buffer, so just switch to the one from the
    standard library.
    
    This makes the algorithm finally O(1) everywhere. \o/

--- clockpro.go
+++ clockpro.go
@@ -15,7 +15,7 @@ It is MIT licensed, like the original.
 package clockpro
 
 import (
-	"container/list"
+	"container/ring"
 	"fmt"
 )
 
@@ -48,23 +48,15 @@ type metaEntry struct {
 	ref   bool
 }
 
-// TODO(dgryski): combine data and metaKeys map
-// TODO(dgryski): advance(elt)
-// TODO(dgryski): container/list -> container/ring
-
 type Cache struct {
 	mem_max  int
 	mem_cold int
-	meta     *list.List
-	metaKeys map[string]*list.Element
+	meta     *ring.Ring
+	metaKeys map[string]*ring.Ring
 
-	hand_pos_hot  *list.Element
-	hand_pos_cold *list.Element
-	hand_pos_test *list.Element
-
-	hand_idx_hot  int
-	hand_idx_cold int
-	hand_idx_test int
+	hand_pos_hot  *ring.Ring
+	hand_pos_cold *ring.Ring
+	hand_pos_test *ring.Ring
 
 	count_hot  int
 	count_cold int
@@ -75,8 +67,7 @@ func New(size int) *Cache {
 	return &Cache{
 		mem_max:  size,
 		mem_cold: size,
-		metaKeys: make(map[string]*list.Element),
-		meta:     list.New(),
+		metaKeys: make(map[string]*ring.Ring),
 	}
 
 }
@@ -141,8 +132,6 @@ func (c *Cache) Set(key string, value interface{}) {
 		c.meta_add(e)
 		c.count_cold++
 	}
-
-	c.VerifyIdxs()
 }
 
 func (c *Cache) meta_add(mentry *metaEntry) {
@@ -151,56 +140,23 @@ func (c *Cache) meta_add(mentry *metaEntry) {
 
 	c.evict()
 
-	if c.hand_pos_hot == nil {
+	if c.meta == nil {
 		// first element
-		elt := c.meta.PushFront(mentry)
+		elt := &ring.Ring{Value: mentry}
+		c.meta = elt
 		c.metaKeys[mentry.key] = elt
 		c.hand_pos_hot = elt
 		c.hand_pos_cold = elt
 		c.hand_pos_test = elt
 	} else {
-		c.VerifyIdxs()
-		c.metaKeys[mentry.key] = c.meta.InsertBefore(mentry, c.hand_pos_hot)
+		elt := &ring.Ring{Value: mentry}
+		c.metaKeys[mentry.key] = elt
+		elt.Link(c.hand_pos_hot)
 
-		if c.hand_idx_cold >= c.hand_idx_hot {
+		if c.hand_pos_cold == c.hand_pos_hot {
 			c.hand_pos_cold = c.hand_pos_cold.Prev()
 		}
-
-		if c.hand_idx_test >= c.hand_idx_hot {
-			c.hand_pos_test = c.hand_pos_test.Prev()
-		}
-
-		c.hand_pos_hot = c.hand_pos_hot.Prev()
 	}
-
-	if c.hand_idx_cold > c.hand_idx_hot {
-		c.hand_idx_cold += 1
-		c.hand_pos_cold = c.hand_pos_cold.Next()
-
-		if c.hand_pos_cold == nil {
-			c.hand_idx_cold = 0
-			c.hand_pos_cold = c.meta.Front()
-		}
-	}
-
-	c.VerifyIdxs()
-
-	if c.hand_idx_test >= c.hand_idx_hot {
-		c.hand_idx_test += 1
-		c.hand_pos_test = c.hand_pos_test.Next()
-		if c.hand_pos_test == nil {
-			c.hand_idx_test = 0
-			c.hand_pos_test = c.meta.Front()
-		}
-	}
-	c.hand_idx_hot += 1
-	c.hand_pos_hot = c.hand_pos_hot.Next()
-	if c.hand_pos_hot == nil {
-		c.hand_idx_hot = 0
-		c.hand_pos_hot = c.meta.Front()
-	}
-
-	c.VerifyIdxs()
 }
 
 func (c *Cache) meta_del(key string) {
@@ -215,67 +171,19 @@ func (c *Cache) meta_del(key string) {
 
 	delete(c.metaKeys, key)
 
-	c.VerifyIdxs()
-
-	var idx int
-
-	// FIXME(dgryski): get rid of this O(n) loop
-
-	for e := c.meta.Front(); e != nil; e = e.Next() {
-		if e == elt {
-			break
-		}
-		idx++
-	}
-
 	if elt == c.hand_pos_hot {
 		c.hand_pos_hot = c.hand_pos_hot.Prev()
-		if c.hand_pos_hot == nil {
-			c.hand_pos_hot = c.meta.Back()
-		}
 	}
 
 	if elt == c.hand_pos_cold {
 		c.hand_pos_cold = c.hand_pos_cold.Prev()
-		if c.hand_pos_cold == nil {
-			c.hand_pos_cold = c.meta.Back()
-		}
 	}
 
 	if elt == c.hand_pos_test {
 		c.hand_pos_test = c.hand_pos_test.Prev()
-		if c.hand_pos_test == nil {
-			c.hand_pos_test = c.meta.Back()
-		}
 	}
 
-	c.meta.Remove(elt)
-
-	max_pos := c.meta.Len() - 1
-
-	if c.hand_idx_hot >= idx {
-		c.hand_idx_hot--
-		if c.hand_idx_hot < 0 {
-			c.hand_idx_hot = max_pos
-		}
-	}
-
-	if c.hand_idx_cold >= idx {
-		c.hand_idx_cold--
-		if c.hand_idx_cold < 0 {
-			c.hand_idx_cold = max_pos
-		}
-	}
-
-	if c.hand_idx_test >= idx {
-		c.hand_idx_test--
-		if c.hand_idx_test < 0 {
-			c.hand_idx_test = max_pos
-		}
-	}
-
-	c.VerifyIdxs()
-
+	elt.Prev().Unlink(1)
 }
 
 func (c *Cache) evict() {
@@ -311,12 +219,7 @@ func (c *Cache) hand_cold() {
 		}
 	}
 
-	c.hand_idx_cold++
 	c.hand_pos_cold = c.hand_pos_cold.Next()
-	if c.hand_pos_cold == nil {
-		c.hand_pos_cold = c.meta.Front()
-		c.hand_idx_cold = 0
-	}
 
 	for c.mem_max-c.mem_cold < c.count_hot {
 		c.hand_hot()
@@ -344,12 +247,7 @@ func (c *Cache) hand_hot() {
 		}
 	}
 
-	c.hand_idx_hot++
 	c.hand_pos_hot = c.hand_pos_hot.Next()
-	if c.hand_pos_hot == nil {
-		c.hand_pos_hot = c.meta.Front()
-		c.hand_idx_hot = 0
-	}
 }
 
 func (c *Cache) hand_test() {
@@ -365,14 +263,8 @@ func (c *Cache) hand_test() {
 	if meta.ptype == ptTest {
 
 		prev := c.hand_pos_test.Prev()
-		pidx := c.hand_idx_test - 1
-		if prev == nil {
-			prev = c.meta.Back()
-			pidx = c.meta.Len()
-		}
 		c.meta_del(meta.key)
 		c.hand_pos_test = prev
-		c.hand_idx_test = pidx
 
 		c.count_test--
 		if c.mem_cold > 1 {
@@ -380,24 +272,20 @@ func (c *Cache) hand_test() {
 		}
 	}
 
-	c.hand_idx_test++
 	c.hand_pos_test = c.hand_pos_test.Next()
-	if c.hand_pos_test == nil {
-		c.hand_pos_test = c.meta.Front()
-		c.hand_idx_test = 0
-	}
 }
 
 func (c *Cache) Dump() {
 
 	if !DEBUG {
 		return
-
 	}
 
 	var b []byte
 
-	for elt := c.meta.Front(); elt != nil; elt = elt.Next() {
+	var end *ring.Ring = nil
+	for elt := c.meta; elt != end; elt = elt.Next() {
+		end = c.meta
 		m := elt.Value.(*metaEntry)
 
 		if c.hand_pos_hot == elt {
@@ -458,7 +346,7 @@ func (c *Cache) FullDump() {
 
 	fmt.Println("-list-")
 	var idx int
-	for elt := c.meta.Front(); elt != nil; elt = elt.Next() {
+	for elt := c.meta; elt != c.meta; elt = elt.Next() {
 		m := elt.Value.(*metaEntry)
 
 		if elt == c.hand_pos_hot {
@@ -471,51 +359,7 @@ func (c *Cache) FullDump() {
 			fmt.Println("TEST pos")
 		}
 
-		if idx == c.hand_idx_hot {
-			fmt.Println("HOT idx")
-		}
-		if idx == c.hand_idx_cold {
-			fmt.Println("COLD idx")
-		}
-		if idx == c.hand_idx_test {
-			fmt.Println("TEST idx")
-		}
-
 		fmt.Printf("%+v\n", m)
 		idx++
-	}
-}
-
-func (c *Cache) VerifyIdxs() {
-
-	if !DEBUG {
-		return
-	}
-
-	if c.meta.Len() == 0 {
-		return
-	}
-
-	hotidx := -1
-	coldidx := -1
-	testidx := -1
-
-	idx := 0
-	for e := c.meta.Front(); e != nil; e = e.Next() {
-		if e == c.hand_pos_hot {
-			hotidx = idx
-		}
-		if e == c.hand_pos_cold {
-			coldidx = idx
-		}
-		if e == c.hand_pos_test {
-			testidx = idx
-		}
-		idx++
-	}
-
-	if hotidx != c.hand_idx_hot || coldidx != c.hand_idx_cold || testidx != c.hand_idx_test {
-		fmt.Println(c.meta.Len(), hotidx, c.hand_idx_hot, coldidx, c.hand_idx_cold, testidx, c.hand_idx_test)
-		panic("index mismatch")
 	}
 }
