e1bafe9b9939286ee0ec46274858ede1ce669230
Damian Gryski <damian@gryski.com>
2014-08-03 02:46:37 +0200

    docs++ (godoc)

--- clockpro.go
+++ clockpro.go
@@ -1,15 +1,16 @@
 // Package clockpro implements the CLOCK-Pro caching algorithm.
 /*
 
-It is based on the python implementation from https://bitbucket.org/SamiLehtinen/pyclockpro .
+CLOCK-Pro is a patent-free alternative to the Adaptive Replacement Cache,
+https://en.wikipedia.org/wiki/Adaptive_replacement_cache .
+It is an approximation of LIRS ( https://en.wikipedia.org/wiki/LIRS_caching_algorithm ),
+much like the CLOCK page replacement algorithm is an approximation of LRU.
+
+This implementation is based on the python code from https://bitbucket.org/SamiLehtinen/pyclockpro .
 
 Slides describing the algorithm: http://fr.slideshare.net/huliang64/clockpro
 
 The original paper: http://static.usenix.org/event/usenix05/tech/general/full_papers/jiang/jiang_html/html.html
-
-This version  uses a circular-linked-list instead of an array to make the
-inserts and deletes O(1) instead of O(n) as in the original python
-implementation.  It also simplifies the bookkeeping considerably.
 
 It is MIT licensed, like the original.
 */
